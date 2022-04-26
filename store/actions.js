0ef99574a43316b57b4975b043ca503eb31565ec
Teerawat Jamlong <teerawat@igeargeek.com>
2022-04-26 16:43:57 +0700

    cart+-

--- store/actions.js
+++ store/actions.js
@@ -16,18 +16,18 @@ export const addProductToCart = ({ commit }, { product, amount }) => {
   commit('ADD_TO_CART', { product, amount })
 }
 
-// export const removeProductFromCart = ({ commit }, product) => {
-//   commit('REMOVE_FROM_CART', product)
-// }
+export const removeProductFromCart = ({ commit }, product) => {
+  commit('REMOVE_FROM_CART', product)
+}//ลบสินค้า 
 
-// export const addAmount = ({ commit }, product) => {
-//   commit('ADD_AMOUNT_PRODUCT', product)
-// }
+export const addAmount = ({ commit }, product) => {
+  commit('ADD_AMOUNT_PRODUCT', product)
+}
 
-// export const reduceAmount = ({ commit }, product) => {
-//   commit('REDUCE_AMOUNT_PRODUCT', product)
-// }
+export const reduceAmount = ({ commit }, product) => {
+  commit('REDUCE_AMOUNT_PRODUCT', product)
+}
 
-// export const emptyCart = ({ commit }) => {
-//   commit('EMPTY_CART')
-// }
+export const emptyCart = ({ commit }) => {
+  commit('EMPTY_CART')
+}
--- store/mutations.js
+++ store/mutations.js
@@ -21,24 +21,24 @@ export const ADD_TO_CART = (state, { product, amount }) => {
   })
 }
 
-// export const REMOVE_FROM_CART = (state, product) => {
-//   state.cart = state.cart.filter(item => item.product.id !== product.id)
-// }
+export const REMOVE_FROM_CART = (state, product) => {
+  state.cart = state.cart.filter(item => item.product.id !== product.id)
+}
 
-// export const ADD_AMOUNT_PRODUCT = (state, product) => {
-//   const currentItem = state.cart.find(item => item.product.id === product.id)
-//   currentItem.amount++
-// }
+export const ADD_AMOUNT_PRODUCT = (state, product) => {
+  const currentItem = state.cart.find(item => item.product.id === product.id)
+  currentItem.amount++
+}
 
-// export const REDUCE_AMOUNT_PRODUCT = (state, product) => {
-//   const currentItem = state.cart.find(item => item.product.id === product.id)
-//   if (currentItem.amount > 1) {
-//     currentItem.amount--
-//   } else {
-//     state.cart = state.cart.filter(item => item.product.id !== product.id)
-//   }
-// }
+export const REDUCE_AMOUNT_PRODUCT = (state, product) => {
+  const currentItem = state.cart.find(item => item.product.id === product.id)
+  if (currentItem.amount > 1) {
+    currentItem.amount--
+  } else {
+    state.cart = state.cart.filter(item => item.product.id !== product.id)
+  }
+}
 
-// export const EMPTY_CART = (state) => {
-//   state.cart = []
-// }
+export const EMPTY_CART = (state) => {
+  state.cart = []
+}
